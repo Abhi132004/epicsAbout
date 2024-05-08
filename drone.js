014292d082017fe08e6fc005e0041dc6dea4cbc3
Abhishek Pokale <98730215+Abhi132004@users.noreply.github.com>
2024-05-08 10:44:26 +0530

    Add files via upload

---
+++ drone.js
@@ -0,0 +1,10 @@
+function drone(){
+    document.getElementById("analytics").style.display = "none";
+    document.getElementById("drone").style.display = "block";
+
+}
+function analytics(){
+    document.getElementById("drone").style.display = "none";
+    document.getElementById("analytics").style.display = "block";
+
+}
